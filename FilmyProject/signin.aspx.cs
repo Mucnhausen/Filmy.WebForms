9a75648031a70ceaceb0da2fa9434efac4287066
Mucnhausen <74404092+Mucnhausen@users.noreply.github.com>
2023-05-23 22:51:42 +0300

    adding successful registration email

--- FilmyProject/signin.aspx.cs
+++ FilmyProject/signin.aspx.cs
@@ -1,4 +1,7 @@
-﻿using System;
+﻿using MailKit.Net.Smtp;
+using MailKit.Security;
+using MimeKit;
+using System;
 using System.Collections.Generic;
 using System.Configuration;
 using System.Data;
@@ -30,9 +33,10 @@ namespace FilmyProject
             else
             {
                 addNewCritic();
+                sendSuccessSignInEmail();
+                clearForm();
                 Response.Write("<script>alert('Sign Up Successful. Go to User Login to Login');</script>");
             }
-            clearForm();
         }
 
         bool ifCriticExists()
@@ -91,6 +95,37 @@ namespace FilmyProject
             countryBox.Text = "";
             phoneBox.Text = "";
             descriptionBox.Text = "";
+        }
+
+
+        void sendSuccessSignInEmail()
+        {
+            MimeMessage message = new MimeMessage();
+            message.From.Add(new MailboxAddress("Filmy Community Help Center", "filmy.community.help@gmail.com"));
+            message.To.Add(new MailboxAddress(usernameBox.Text.Trim(), emailBox.Text.Trim()));
+
+            message.Subject = "Welcome, " + first_nameBox.Text.Trim();
+
+            message.Body = new TextPart("html")
+            {
+                Text = "<!DOCTYPE html><html><head>  <meta charset=\"UTF-8\">  <meta name=\"viewport\" content=\"width=device-width, initial-scale=1.0\">  <title>Welcome to our site!</title>  <style>    /* Global CSS styles */    body {      font-family: Arial, sans-serif;      background-color: #f5f5f5;    }    /* Container styles */    .container {      max-width: 600px;      margin: 0 auto;      padding: 20px;      background-color: #f2eaff;      border-radius: 5px;      box-shadow: 0 2px 5px rgba(0, 0, 0, 0.1);    }    /* Heading styles */    h1 {      color: #3a256d;      margin-top: 0;      text-align: center;    }    /* Content styles */    p {      color: #5d487f;      line-height: 1.5;    }    /* Button styles */    .button {      display: inline-block;      padding: 10px 20px;      background-color: #693ba3;      color: #ffffff;      text-decoration: none;      border-radius: 4px;    }    /* Footer styles */    .footer {      text-align: center;      margin-top: 20px;      color: #999999;    }  </style></head><body>  <div class=\"container\">    <h1>Welcome to Filmy!</h1>    <p>Dear " + first_nameBox.Text.Trim() + " ,</p>    <p>Thank you for registering on our site. We are thrilled to have you as a part of our community of movie enthusiasts!</p>    <p>As a registered movie critic, you'll have the opportunity to share your valuable reviews with our audience.</p>    <p>Start exploring our site today and let us know if you have any questions or need assistance. We're here to help!</p>    <p>      <a style=\"color: white\" class=\"button\" href=\"mailto:filmy.community.help@gmail.com?subject=" + usernameBox.Text.Trim() + "&body={Describe the encountered problem}\">Ask for support</a>    </p>    <div class=\"footer\">      <p>Best regards,</p>      <p>The Filmy Team</p>    </div>  </div></body></html>"
+            };
+
+
+            SmtpClient client = new SmtpClient();
+
+            try
+            {
+                client.Connect("smtp.gmail.com", 587, SecureSocketOptions.StartTls);
+                client.Authenticate("filmy.community.help@gmail.com", "doadkcgpdoymsyjh");
+                client.Send(message);
+            }
+            catch (Exception ex) { Response.Write("<script>alert(' " + ex.Message + " ');</script>"); }
+            finally
+            {
+                client.Disconnect(true);
+                client.Dispose();
+            }
         }
     }
 }
