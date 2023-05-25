6b3d2ad1d7ce38582fdf47d56dfade88e86039d0
Mucnhausen <74404092+Mucnhausen@users.noreply.github.com>
2023-05-25 18:13:20 +0300

    movies management functionality

---
+++ FilmyProject/moviesManagment.aspx
@@ -0,0 +1,87 @@
+﻿<%@ Page Title="" Language="C#" MasterPageFile="~/Filmy.Master" AutoEventWireup="true" CodeBehind="moviesManagment.aspx.cs" Inherits="FilmyProject.moviesManagment" %>
+<asp:Content ID="Content1" ContentPlaceHolderID="head" runat="server">
+    <link href="css/criticsManagment.css" rel="stylesheet" />
+</asp:Content>
+
+<asp:Content ID="Content2" ContentPlaceHolderID="ContentPlaceHolder1" runat="server">
+    <div class="content">
+        <div class="main">
+            <div class="main-form">
+                <div class="slogan">
+                    <h1 class="slogan-text">You are the power!</h1>
+                </div>
+                <div class="form" action="">
+                    <div class="row">
+                        <asp:TextBox ID="titleBox" runat="server" class="input2" placeholder="Title"></asp:TextBox>
+                    </div>
+                    <div class="row">
+                        <asp:TextBox ID="idBox" runat="server" class="input3" placeholder="ID"></asp:TextBox>
+                        <asp:Button ID="findBtn" runat="server" class="find margin-left" Text="Find" OnClick="findBtn_Click"/>
+                    </div>
+                    <div class="row">
+                        <asp:TextBox ID="dateBox" runat="server" TextMode="Date" class="input2 date" title="Publishing date"></asp:TextBox>
+                    </div>
+                    <div class="row">
+                        <asp:TextBox ID="budgetBox" runat="server" class="input2" placeholder="Budget"></asp:TextBox>
+                    </div>
+                    <div class="row">
+                        <asp:TextBox ID="ratingBox" runat="server" class="input2" placeholder="Rating"></asp:TextBox>
+                    </div>
+                    <div class="row textarea">
+                        <asp:TextBox ID="genresBox" runat="server" TextMode="MultiLine" Rows="4" class="input2" placeholder="Genres (',' - separator)"></asp:TextBox>
+                    </div>
+                    <div class="row textarea">
+                        <asp:TextBox ID="actorsBox" runat="server" TextMode="MultiLine" Rows="4" class="input2" placeholder="Actors (',' - separator)"></asp:TextBox>
+                    </div>
+                    <div class="row textarea">
+                        <asp:TextBox ID="producersBox" runat="server" TextMode="MultiLine" Rows="4" class="input2" placeholder="Producers (',' - separator)"></asp:TextBox>
+                    </div>
+                    <div class="row textarea">
+                        <asp:TextBox ID="descriptionBox" runat="server" TextMode="MultiLine" Rows="4" class="input2" placeholder="Tell about yourself"></asp:TextBox>
+                    </div>
+                    <div class="row buttons">
+                        <asp:Button ID="addBtn" runat="server" class="input2 add" Text="Add" OnClick="addBtn_Click"/>
+                        <asp:Button ID="updateBtn" runat="server" class="input2 update" Text="Update" OnClick="updateBtn_Click"/>
+                        <asp:Button ID="deleteBtn" runat="server" class="input2 delete" Text="Delete" OnClick="deleteBtn_Click"/>
+                    </div>
+                </div>
+            </div>
+            <div class="main-table-wrapper">
+                <asp:SqlDataSource ID="SqlDataSource1" runat="server" ConnectionString="<%$ ConnectionStrings:ConnectionString %>" SelectCommand="SELECT * FROM [movies]"></asp:SqlDataSource>
+                <asp:GridView ID="GridView1" runat="server" DataSourceID="SqlDataSource1" class="table" style="background-color: white;" AutoGenerateColumns="False" DataKeyNames="Id">
+                    <Columns>
+                        <asp:BoundField DataField="Id" HeaderText="Id" ReadOnly="True" SortExpression="Id" />
+                        <asp:BoundField DataField="movie_name" HeaderText="movie_name" SortExpression="movie_name" />
+                        <asp:BoundField DataField="date" HeaderText="date" SortExpression="date" />
+                        <asp:BoundField DataField="genres" HeaderText="genres" SortExpression="genres" />
+                        <asp:BoundField DataField="actors" HeaderText="actors" SortExpression="actors" />
+                        <asp:BoundField DataField="producers" HeaderText="producers" SortExpression="producers" />
+                        <asp:BoundField DataField="rating" HeaderText="rating" SortExpression="rating" />
+                        <asp:BoundField DataField="description" HeaderText="description" SortExpression="description" />
+                        <asp:BoundField DataField="budget" HeaderText="budget" SortExpression="budget" />
+                    </Columns>
+                </asp:GridView> <%--Table--%>
+                <div class="main-table-item">
+<%--                    <table id="myTable" class="display table">
+                    <thead>
+                        <tr>
+                            <th>Column 1</th>
+                            <th>Column 2</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        <tr>
+                            <td>Row 1 Data 1</td>
+                            <td>Row 1 Data 2</td>
+                        </tr>
+                        <tr>
+                            <td>Row 2 Data 1</td>
+                            <td>Row 2 Data 2</td>
+                        </tr>
+                    </tbody>
+                </table>--%>
+                </div>
+            </div>
+        </div>
+    </div>
+</asp:Content>
---
+++ FilmyProject/moviesManagment.aspx.cs
@@ -0,0 +1,224 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace FilmyProject
+{
+    public partial class moviesManagment : System.Web.UI.Page
+    {
+        SqlConnection con = new SqlConnection(ConfigurationManager.ConnectionStrings["ConnectionString"].ConnectionString);
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if (Session["role"].ToString() == "visitor" || Session["role"].ToString() == "critic")
+            {
+                Response.Write("You have no rights to view the content of that page");
+                Response.End();
+            }
+            DataBind();
+        }
+
+        protected void findBtn_Click(object sender, EventArgs e)
+        {
+            if (ifMovieExists())
+            {
+                getMovieById();
+            } else {
+                Response.Write("<script>alert('ID does not match any movie');</script>");
+            }
+        }
+
+        protected void addBtn_Click(object sender, EventArgs e)
+        {
+            if (ifTitleExists())
+            {
+                Response.Write("<script>alert('Movie title already exists');</script>");
+            }
+            else
+            {
+                addNewMovie();
+                clearForm();
+                Response.Write("<script>alert('Movie added successfully.');</script>");
+                DataBind();
+                
+            }
+        }
+
+        protected void updateBtn_Click(object sender, EventArgs e)
+        {
+            if (ifMovieExists())
+            {
+                if (ifTitleExists())
+                {
+                    Response.Write("<script>alert('Movie title already exists');</script>");
+                } else {
+                    updateMovie();
+                    Response.Write("<script>alert('Movie updated successfully.');</script>");
+                    DataBind();
+                }
+                
+            } else {
+                Response.Write("<script>alert('ID does not match any movie');</script>");
+            }
+        }
+
+        protected void deleteBtn_Click(object sender, EventArgs e)
+        {
+            if (ifMovieExists())
+            {
+                deleteMovie();
+                clearForm();
+                Response.Write("<script>alert('Movie deleted successfully.');</script>");
+                DataBind();
+            }
+            else
+            {
+                Response.Write("<script>alert('ID does not match any movie');</script>");
+            }
+        }
+        void getMovieById()
+        {
+            try
+            {
+                con.Open();
+                SqlCommand cmd = new SqlCommand("SELECT * FROM movies WHERE Id=@id", con);
+                cmd.Parameters.AddWithValue("@id", idBox.Text.Trim());
+                SqlDataReader reader = cmd.ExecuteReader();
+
+                if (reader.Read())
+                {
+                    titleBox.Text = reader["movie_name"].ToString();
+                    genresBox.Text = reader["genres"].ToString();
+                    actorsBox.Text = reader["actors"].ToString();
+                    producersBox.Text = reader["producers"].ToString();
+                    dateBox.Text = DateTime.Parse(reader["date"].ToString()).ToString("yyyy-MM-dd");
+                    ratingBox.Text = reader["rating"].ToString();
+                    budgetBox.Text = reader["budget"].ToString();
+                    descriptionBox.Text = reader["description"].ToString();
+
+                    // Use the retrieved data as needed (e.g., display it in your ASP.NET Web Forms page)
+                }
+                reader.Close();
+                cmd.ExecuteNonQuery();
+                con.Close();
+            }
+            catch (Exception ex)
+            {
+                Response.Write("<script>alert('" + ex.Message + "');</script>");
+            }
+        }
+
+        bool ifMovieExists()
+        {
+            try
+            {
+                using (SqlCommand sqlCommand = new SqlCommand("SELECT COUNT(*) from movies where Id = @id", con))
+                {
+                    con.Open();
+                    sqlCommand.Parameters.AddWithValue("@id", idBox.Text.Trim());
+                    int userCount = (int)sqlCommand.ExecuteScalar();
+                    con.Close();
+                    if (userCount > 0) { return true; } else { return false; }
+                }
+
+            }
+            catch (Exception ex) { Response.Write("<script>alert('An error occured. Try later \n " + ex.Message + " ');</script>"); return false; }
+        }
+
+        bool ifTitleExists()
+        {
+            try
+            {
+                using (SqlCommand sqlCommand = new SqlCommand("SELECT COUNT(*) from movies where movie_name = @title", con))
+                {
+                    con.Open();
+                    sqlCommand.Parameters.AddWithValue("@title", titleBox.Text.Trim());
+                    int userCount = (int)sqlCommand.ExecuteScalar();
+                    con.Close();
+                    if (userCount > 0) { return true; } else { return false; }
+                }
+
+            }
+            catch (Exception ex) { Response.Write("<script>alert('An error occured. Try later \n " + ex.Message + " ');</script>"); return false; }
+        }
+        void addNewMovie()
+        {
+            try
+            {
+                con.Open();
+                SqlCommand cmd = new SqlCommand("INSERT INTO movies(movie_name,date,genres,actors,producers,rating,description,budget) " +
+                    "values(@title,@date,@genres,@actors,@producers,@rating,@description,@budget)", con);
+                cmd.Parameters.AddWithValue("@title", titleBox.Text.Trim());
+                cmd.Parameters.AddWithValue("@genres", genresBox.Text.Trim());
+                cmd.Parameters.AddWithValue("@actors", actorsBox.Text.Trim());
+                cmd.Parameters.AddWithValue("@date", DateTime.Parse(dateBox.Text.Trim()).ToString("dd\\/MM\\/yyyy"));
+                cmd.Parameters.AddWithValue("@producers", producersBox.Text.Trim());
+                cmd.Parameters.AddWithValue("@rating", ratingBox.Text.Trim());
+                cmd.Parameters.AddWithValue("@budget", budgetBox.Text.Trim());
+                cmd.Parameters.AddWithValue("@description", descriptionBox.Text.Trim());
+                cmd.ExecuteNonQuery();
+                con.Close();
+            }
+            catch (Exception ex)
+            {
+                Response.Write("<script>alert('" + ex.Message + "');</script>");
+            }
+        }
+        void updateMovie()
+        {
+            try
+            {
+                con.Open();
+                SqlCommand cmd = new SqlCommand("UPDATE movies SET movie_name=@title, genres=@genres, actors=@actors, producers=@producers, description=@description, date=@date, rating=@rating, budget=@budget WHERE Id=@id", con);
+                cmd.Parameters.AddWithValue("@id", idBox.Text.Trim());
+                cmd.Parameters.AddWithValue("@title", titleBox.Text.Trim());
+                cmd.Parameters.AddWithValue("@genres", genresBox.Text.Trim());
+                cmd.Parameters.AddWithValue("@actors", actorsBox.Text.Trim());
+                cmd.Parameters.AddWithValue("@date", DateTime.Parse(dateBox.Text.Trim()).ToString("dd\\/MM\\/yyyy"));
+                cmd.Parameters.AddWithValue("@producers", producersBox.Text.Trim());
+                cmd.Parameters.AddWithValue("@rating", ratingBox.Text.Trim());
+                cmd.Parameters.AddWithValue("@budget", budgetBox.Text.Trim());
+                cmd.Parameters.AddWithValue("@description", descriptionBox.Text.Trim());
+                cmd.ExecuteNonQuery();
+                con.Close();
+            }
+            catch (Exception ex)
+            {
+                Response.Write("<script>alert('" + ex.Message + "');</script>");
+            }
+        }
+
+        void deleteMovie()
+        {
+            try
+            {
+                con.Open();
+                SqlCommand cmd = new SqlCommand("DELETE FROM movies WHERE Id = @id", con);
+                cmd.Parameters.AddWithValue("@id", idBox.Text.Trim());
+                cmd.ExecuteNonQuery();
+                con.Close();
+            }
+            catch (Exception ex)
+            {
+                Response.Write("<script>alert('" + ex.Message + "');</script>");
+            }
+        }
+
+        void clearForm()
+        {
+            titleBox.Text = "";
+            idBox.Text = "";
+            dateBox.Text = "";
+            budgetBox.Text = "";
+            genresBox.Text = "";
+            actorsBox.Text = "";
+            producersBox.Text = "";
+            descriptionBox.Text = "";
+        }
+    }
+}
